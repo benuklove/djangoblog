ef19e1847ffd59b3151715c321c1a1798a3bbf13
benlove <benuklove@gmail.com>
2018-02-07 14:47:37 -0600

    Convert posts to class based view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -19,6 +20,14 @@ def post_list(request):
     return render(request, 'blog/post/list.html',{
         'page': page, 'posts': posts
         })
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 # A view to display a single post
 def post_detail(request, year, month, day, post):
