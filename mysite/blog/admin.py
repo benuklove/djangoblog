1f33f516fd0149f58a8ae149f96bab9e37abefa3
benlove <benuklove@gmail.com>
2018-02-18 13:45:33 -0600

    Sync db and apply Comment migrations

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish','status')
@@ -14,3 +15,11 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
 
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Comment, CommentAdmin)
